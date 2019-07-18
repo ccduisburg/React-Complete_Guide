1b73eb11df4ff07e41de47e01b8a0b2499bc3d0a
kabakcic <c.kabakci@ddg-online.de>
2019-07-18 17:28:26 +0200

    second git commit -m second

--- src/App.js
+++ src/App.js
@@ -33,29 +33,49 @@ class  App  extends Component{
                     })
                                    }
 
-          
+          deletePersonHandler=(personIndex)=>{
+                const persons=[...this.state.persons];
+                persons.splice(personIndex,1);
+                this.setState({persons:persons})
+                 }
+
           togglePersonsHandler=()=>{
            const doesShow=this.state.showPersons;
            this.setState({showPersons:!doesShow});
           }
+
 render(){
+
+  let persons=null;
+  if(this.state.showPersons){
+    persons=(
+      <div>
+        {this.state.persons.map((person,index)=>{
+          return <Person 
+          click={()=>this.deletePersonHandler(index)}
+          name={person.name}
+          age={person.age}/>
+        })}       
+        </div>
+    );
+
+  }
+
   return (
     <div className="App">
      <h1>Hi, I'm aReact App</h1>
         <p>This is really working!</p>
-        <button onClick={this.togglePersonsHandler}>Toogle Persons</button>
-        { this.state.showPersons===true ? 
-        <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} 
-        click={this.switchNameHandler.bind(this,'maaaax')} changed={this.nameChangeHandler}>My hobies:Racing </Person>    
-        </div>:null
-        }
-        
+        <button onClick={this.togglePersonsHandler}>Toogle Persons</button>              
     </div>
   );
   }
 }
  
 export default App;
+/**
+ *  <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} 
+        click={this.switchNameHandler.bind(this,'maaaax')} changed={this.nameChangeHandler}>My hobies:Racing </Person>    
+        {persons}
+ */
